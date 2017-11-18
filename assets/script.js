7bcce349b1d4d9d9f73f845b7410364bc6ee764f
Brandon Berookhim <brandonberookhim@gmail.com>
2017-11-17 19:10:58 -0800

    cleaner code

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,24 @@
 $(document).ready(function() {
 	let html = document.getElementsByTagName('html')[0];
-	var og = true;
 
-	function change(a, b, o, p, d, ch) {
+	function change(a, b, o, ch) {
 		html.style.setProperty("--b", a);
 		html.style.setProperty("--w", b);
 		html.style.setProperty("--opac", o);
-		html.style.setProperty("--on", p);
-		html.style.setProperty("--off", d);
-		$.each($(".turnoff"), function() { $(this).html(ch.repeat($(this).html().length))});
+		$.each($(".turnoff"), function() { $(this).html(ch.repeat($(this).html().length)) });
 	}
 
-	$(".turnoff").click(function () {
-		change("black", "white", .95, "pointer", "default", "0");
-		og = true;
-	});
+	function on() {
+		change("white", "black", .87, "O");
+	}
 
-	$(".turnoff").hover(
-		function () { if (!og)  { change("black", "white", .95, "default", "pointer", "0"); }},
-		function () { if (!og) { change("white", "black", .87, "default", "pointer", "O"); }}
-	);
+	function off() {
+		change("black", "white", .95, "0");
+	}
 
-	$(".turnon").click(function () {
-		change("white", "black", .87, "default", "pointer", "O");
-		og = false;
-	});
+	$(".turnoff").click(off);
+	$(".turnoff").hover(off);
+	$(".turnon").click(on);
+	$(".turnon").hover(on);
 
-	$(".turnon").hover(
-		function () { if (og) { change("white", "black", .87, "pointer", "default", "O"); }},
-		function () { if (og)  { change("black", "white", .95, "pointer", "default", "0"); }}
-	);
-	
 });
