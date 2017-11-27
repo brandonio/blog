144258fbeb7670f4975aa0a83b6182b3f8168525
Brandon Berookhim <brandonberookhim@gmail.com>
2017-11-26 23:58:01 -0800

    info

--- assets/script.js
+++ assets/script.js
@@ -5,18 +5,23 @@ $(document).ready(function() {
 
 	let og = true;
 
+
 	function change(a, b, o, ch, e) {
 		html.style.setProperty("--b", a);
 		html.style.setProperty("--w", b);
 		html.style.setProperty("--opac", o);
+		// jQuery Function Number 1
 		$.each($(".turnoff"), function() { $(this).html(ch.repeat($(this).html().length)) });
 		if (ind || huevo) {
 			if (ind) {
-				$.each($(".ze"), function() { $(this).html(ch.repeat($(this).html().length)) });
+				// jQuery Function Number 2
+				$.each($(".ze"), function() { $(this).text(ch.repeat($(this).text().length)) });
 			}
 			if (e) {
+				// jQuery Function Number 3
 				$("#egg").show();
 			} else {
+				// jQuery Function Number 4
 				$("#egg").hide();
 			}
 		}
@@ -34,8 +39,9 @@ $(document).ready(function() {
 			change("black", "white", .95, "0", false);
 		}
 	}
-
+	// jQuery Function Number 5
 	$(".turnoff").click(off);
+	// jQuery Function Number 6
 	$(".turnoff").hover(off);
 	$(".turnon").click(on);
 	$(".turnon").hover(on);
