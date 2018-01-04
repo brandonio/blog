406d731be5eb8b6e2cb18f7f37945f2621cbe0d7
Brandon Berookhim <brandonberookhim@gmail.com>
2018-01-03 17:09:46 -0800

    omg i forgot viewport

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,17 @@ $(document).ready(function() {
 	const html = document.getElementsByTagName('html')[0];
 	const ind = $('.ze').length > 0;
 	const huevo = $("#egg").length > 0;
+	const mob = isMobile();
 
 	let og = true;
-	// let switched = false;
-	// let other = false;
 
 	function change(a, b, o, ch, e) {
 		html.style.setProperty("--b", a);
 		html.style.setProperty("--w", b);
 		html.style.setProperty("--opac", o);
-		$.each($(".turnoff"), function() { $(this).html(ch.repeat($(this).html().length)) });
+		// $.each($(".turnoff"), function() { $(this).html(ch.repeat($(this).html().length)) });
 		if (ind || huevo) {
-			if (ind) { $.each($(".ze"), function() { $(this).html(ch.repeat($(this).html().length)) }); }
+			if (!mob && ind) { $.each($(".ze"), function() { $(this).html(ch.repeat($(this).html().length)) }); }
 			if (e) { $("#egg").show(); }
 			else { $("#egg").hide(); }
 		}
@@ -23,10 +22,22 @@ $(document).ready(function() {
 	function on() { if (og) { change("white", "black", .87, "O", true); }}
 	function off() { if (!og) { change("black", "white", .94, "0", false); }}
 
-	$(".turnoff").click(off);
-	$(".turnoff").hover(off);
-	$(".turnon").click(on);
-	$(".turnon").hover(on);
+	function isMobile() {
+  		try{ document.createEvent("TouchEvent"); return true; }
+  		catch(e) { return false; }
+	}
+
+	if (mob) {
+		$(".turnoff").click(off);
+		$(".turnon").click(on);
+		$("#binswitch").click(showHex);
+		$("#hexswitch").click(showBin);
+	} else {
+		$(".turnoff").hover(off);
+		$(".turnon").hover(on);
+		$("#hexswitch").hover(showBin);
+		$("#binswitch").hover(showHex);
+	}
 
 	function showHex() {
 		if ($("#longname").css("display") == "block") {
@@ -37,45 +48,8 @@ $(document).ready(function() {
 
 	function showBin() {
 		if ($("#longname").css("display") == "block") {
-			// switched = true;
-			// other = true;
-			// $(".turnoff").off();
-			// $(".turnon").off();
 			$("#longnum").removeClass("hidelong");
 			$("#shortnum").removeClass("showshort");
-			// $(".turnoff").click(addback);
-			// $(".turnoff").hover(addback);
-			// $(".turnon").click(addbackother);
-			// $(".turnon").hover(addbackother);
-			// switched = false;
-			// other = false;
 		}
 	}
-
-	// function addback() {
-	// 	if (switched) { switched = false; }
-	// 	else {
-	// 		$(".turnoff").off();
-	// 		$(".turnoff").click(off);
-	// 		$(".turnoff").hover(off);
-	// 		if (og) { off(); }
-	// 		// if (og) { on(); } else { off(); }
-	// 	}
-	// }
-
-	// function addbackother() {
-	// 	if (other) { other = false; }
-	// 	else {
-	// 		$(".turnon").off();
-	// 		$(".turnon").click(on);
-	// 		$(".turnon").hover(on);
-	// 		if (!og) { on(); }
-	// 		// if (og) { on(); } else { off(); }
-	// 	}
-	// }
-
-	$("#binswitch").click(showHex);
-	$("#binswitch").hover(showHex);
-	$("#hexswitch").click(showBin);
-	$("#hexswitch").hover(showBin);
 });
